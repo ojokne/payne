40041fae072a926deda5b1292d122e323af4e16b
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2025-05-06 05:29:44 +0300

    Feature(frontend): Add font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,29 @@
-import '@coinbase/onchainkit/styles.css';
-import type { Metadata } from 'next';
-import './globals.css';
-import { Providers } from '@/components/providers/providers';
+import type { Metadata } from "next";
+import "./globals.css";
+import { Providers } from "@/components/providers/providers";
+// import "@coinbase/onchainkit/styles.css";
+import {
+  Inter,
+  Manrope,
+} from "next/font/google";
+
+// Load Inter
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+// Load Manrope
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Payne",
-  description: 'dApp that merchants use to recieve USDC Payments',
+  description: "dApp that merchants use to recieve USDC Payments",
 };
 
 export default function RootLayout({
@@ -14,8 +32,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-background dark">
+    <html lang="en" className={`${inter.variable} ${manrope.variable}`}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body
+       className="font-body"
+       >
         <Providers>{children}</Providers>
       </body>
     </html>
